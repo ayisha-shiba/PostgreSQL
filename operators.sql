fae9f0e962742bb5816522bae39e68dafc07332c
ayisha-shiba <ayishashiba.p@gmail.com>
2025-10-31 21:38:58 +0530

    Added min,max,avg

---
+++ operators.sql
@@ -0,0 +1,29 @@
+postgres=# SELECT AVG(price) FROM CAR;
+        avg
+--------------------
+ 47698.982100000000
+(1 row)
+
+
+postgres=# SELECT MAX(price) FROM CAR;
+   max
+----------
+ 99003.41
+(1 row)
+
+
+postgres=# SELECT MIN(price) FROM CAR;
+   min
+---------
+ 2318.53
+(1 row)
+
+
+postgres=# SELECT AVG(price) FROM CAR;
+        avg
+--------------------
+ 47698.982100000000
+(1 row)
+
+
+postgres=#
